8480d7db6c7db53417737d530a3d46afd39e4a98
MedsaathiAi <ibadkhan931@gmail.com>
2026-07-16 17:28:54 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
     console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
 console.log("SECRET_EXISTS:", !!process.env.RAZORPAY_KEY_SECRET);
     const auth = Buffer.from(
-  ${process.env.RAZORPAY_KEY_ID}:${process.env.RAZORPAY_KEY_SECRET}
+  `${process.env.RAZORPAY_KEY_ID}:${process.env.RAZORPAY_KEY_SECRET}`
 ).toString("base64");
 
     const response = await fetch("https://api.razorpay.com/v1/orders", {
